1a8072dac15ece0bc8c1639df21a5ab71452f22c
Cameron Lund <clund6314@gmail.com>
2021-06-24 19:08:14 -0600

    added controllers and controller action to checkouts. Created checkouts,checkoutform, and checkoutlist and began creating the crud actions.

--- client/src/index.js
+++ client/src/index.js
@@ -7,20 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { initMiddleware } from 'devise-axios';
 import AuthProvider from './providers/AuthProvider';
-
+import CheckoutProvider from './providers/CheckoutProvider';
 initMiddleware();
-
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <CheckoutProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CheckoutProvider>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
